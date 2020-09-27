f8f5bb396c38f02572e8f4d1fe3973f7c2d1bcc2
Amitha Raghava Raju <araghav4@wolftech.ad.ncsu.edu>
2020-09-27 15:09:56 -0400

    Heroku confiured

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/amithawebsite'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/amithawebsite/index.html'));
 });
 console.log('Console listening!');
